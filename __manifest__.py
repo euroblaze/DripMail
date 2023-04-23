4b2eafba2419ef1370be1a8809392cdccfda7ef4
Simplify-ERP® <github@euroblaze.de>
2023-04-23 09:52:59 +0200

    Create __manifest__.py

---
+++ __manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Drip Marketing Tool',
+    'version': '1.0',
+    'category': 'Marketing',
+    'summary': 'Create and manage email chains for drip marketing campaigns',
+    'author': 'Simplify-ERP™',
+    'website': 'https://simplify-erp.de',
+    'depends': ['mail'],
+    'data': [
+        'views/views.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
